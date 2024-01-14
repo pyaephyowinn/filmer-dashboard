b7e1fe9631a9fcbce4b71ad3f826cabaae826af6
pyaephyowin <pyaephyowinnn@gmail.com>
2024-01-14 23:33:24 +0630

    add axios response interceptor

--- src/hooks/useAppSetup.ts
+++ src/hooks/useAppSetup.ts
@@ -9,7 +9,7 @@ export function useAppSetup() {
 
   useEffect(() => {
     setupRequestInterceptor();
-    // setupResponseInterceptor();
+    setupResponseInterceptor();
     setInit(true);
   }, []);
 
